550e0b22be71ef3e9b4f0d78b91b37a06150d64e
candyslug <david.wip.long@gmail.com>
2022-05-04 16:12:12 +0800

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(data) {
+    console.log("->" + data);
+    console.log(data[0]["title"]);
+    let ret = []
+    for (let i = 0; i < data.length; i++) {
+        ret.push(data[i]["title"]);
+    }
+    
+    return ret;
 };
 
 // Do not edit below this line
