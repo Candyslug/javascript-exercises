e2fed78d8de2ee8bbf0df48a7712db0b1042186b
candyslug <david.wip.long@gmail.com>
2022-05-04 15:07:15 +0800

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let a = "";
+    for (var i = s.length-1; i >= 0; i--) {
+        a += s[i];
+    }
+    return a;
 };
 
 // Do not edit below this line
