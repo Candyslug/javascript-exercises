2a64387d1a570318c8cab37ab9e75d17476e42ee
candyslug <david.wip.long@gmail.com>
2022-05-04 16:20:37 +0800

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const calcAge = function (yob, yod) {
+    return yod - yob;
+}
+
+const findTheOldest = function (data) {
+
+    let a = calcAge(
+        data[0]["yearOfBirth"],
+        data[0]["yearOfDeath"]
+    );
+
+    let n = 0;
+    for (var i = 0; i < data.length; i++) {
+        if (calcAge(
+                data[i]["yearOfBirth"],
+                data[i]["yearOfDeath"]) > a) {
+            a = calcAge(
+                data[i]["yearOfBirth"],
+                data[i]["yearOfDeath"]);
+            n = i;
+        }
+    }
+    return data[n];
 
 };
 
