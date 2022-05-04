7d2d4de98b35a6253d51c365b2389ca7152c9ce1
candyslug <david.wip.long@gmail.com>
2022-05-04 16:01:13 +0800

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
 const palindromes = function () {
-
+    let s = arguments[0];
+    let first = "";
+    let last = "";
+    for (let i = 0; i < s.length; i++) {
+        first += s[i];
+    }
+    for (let i = s.length-1; i >= 0; i--) {
+        last += s[i];
+    }
+    console.log(first + " or " + last);
+    return (first == last);
 };
 
 // Do not edit below this line
