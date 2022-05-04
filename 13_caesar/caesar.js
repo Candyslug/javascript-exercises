0d7399f774d9bf0f634c4456823863eeac236266
candyslug <david.wip.long@gmail.com>
2022-05-04 16:34:07 +0800

    finished exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,6 @@
-const caesar = function() {
-
+const caesar = function(a, i) {
+    let b = String.prototype.charCodeAt.call(a);
+    return String.fromCharCode(b + i);
 };
 
 // Do not edit below this line
