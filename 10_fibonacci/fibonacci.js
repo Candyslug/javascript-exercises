7b3b6435d036c35b63732bc894064c925c048432
candyslug <david.wip.long@gmail.com>
2022-05-04 16:05:52 +0800

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = function() {
-
+    let n = arguments[0];
+    console.log("-> " + n);
+    let term1 = 0;
+    let term2 = 1;
+    let nextTerm = term1 + term2;
+    for (let i = 2; i < n; i++) {
+        term1 = term2;
+        term2 = nextTerm;
+        nextTerm = term1 + term2;
+    }
+    return (nextTerm);
 };
 
 // Do not edit below this line
